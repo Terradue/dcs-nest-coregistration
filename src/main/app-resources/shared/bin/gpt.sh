fdbdc7dbc87a039fb49e656a7b65571c1a0b93f3
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-07-24 17:40:30 +0200

    Nest operators available with gpt.sh
    Signed-off-by: Emmanuel Mathot <emmanuel.mahot@gmail.com>

---
+++ src/main/app-resources/shared/bin/gpt.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+export NEST_HOME=$_CIOP_APPLICATION_PATH/shared
+
+if [ -z "$NEST_HOME" ]; then
+    echo
+    echo Error: NEST_HOME not found in your environment.
+    echo Please set the NEST_HOME variable in your environment to match the
+    echo location of the NEST 5.x installation
+    echo
+    exit 2
+fi
+
+. "$NEST_HOME/bin/detect_java.sh"
+
+$app_java_home/bin/java \
+	-server -Xms512M -Xmx3000M -XX:PermSize=512m -XX:MaxPermSize=512m -Xverify:none \
+    -XX:+AggressiveOpts -XX:+UseFastAccessorMethods \
+    -XX:+UseParallelGC -XX:+UseNUMA -XX:+UseLoopPredicate -XX:+UseStringCache \
+    -Dceres.context=nest \
+    "-Dnest.mainClass=org.esa.beam.framework.gpf.main.Main" \
+    "-Dnest.home=$NEST_HOME" \
+	"-Dnest.debug=false" \
+    "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=$NEST_HOME/lib/libjhdf.so" \
+    "-Dncsa.hdf.hdf5lib.H5.hdf5lib=$NEST_HOME/lib/libjhdf5.so" \
+    -jar $NEST_HOME/lib/ceres-launcher.jar "$@"
+
+exit 0
