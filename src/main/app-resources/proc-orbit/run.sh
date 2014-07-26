69577b8d335eea1b0f06742c9a968545d8ed3dc0
Fabrice Brito <fabrice.brito@terradue.com>
2014-07-26 14:07:14 +0200

    Create run.sh

---
+++ src/main/app-resources/proc-orbit/run.sh
@@ -0,0 +1,47 @@
+!/bin/bash
+
+# source the ciop functions (e.g. ciop-log)
+source ${ciop_job_include}
+
+# define the exit codes
+SUCCESS=0
+ERR_GPT=10
+
+# add a trap to exit gracefully
+function cleanExit ()
+{
+   local retval=$?
+   local msg=""
+   case "$retval" in
+     $SUCCESS) msg="Processing successfully concluded";;
+     $ERR_GPT) msg="gpt returned an error";;
+     *) msg="Unknown error";;
+   esac
+   [ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
+   exit $retval
+}
+
+trap cleanExit EXIT
+
+# Apply-Orbit-File parameters
+orbitType="`ciop-getparam orbitType`"
+
+# loop through the inputs
+while read input
+do
+
+  local_input=`echo $input | ciop-copy -o $TMPDIR/input -`
+  base_input=`basename $local_input | sed "s/.N1//"`
+  
+  /application/shared/bin/gpt.sh Apply-Orbit-File \
+    -PorbitType=$orbitType \
+    -t $TMPDIR/output ${base_input}.dim \
+    $local_input
+  
+  [ $? != 0 ] && exit $ERR_GPT 
+    
+  tar -C $TMPDIR/output -f ${base_input}.tgz -cz  ${base_input}.d*
+  
+  ciop-publish $TMPDIR/output/${base_input}.tgz
+  
+done
